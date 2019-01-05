de6a9e56b4e9257710a02f95507c042d7acf0fb8
Issac Chan <issaczhaoming.chan@my.jcu.edu.au>
2019-01-06 01:59:53 +0800

    comment out unnecessary content

--- song.py
+++ song.py
@@ -1,7 +1,7 @@
 # create your Song class in this file
 class Song:
     #song.py properties
-    def __init__(self, song_Title = '', song_Artist = '', song_Year = 0000, song_Required = True):
+    def __init__(self, song_Title = '', song_Artist = '', song_Year = 0000, song_Required = False):
         self.title = song_Title
         self.artist = song_Artist
         self.year = song_Year
@@ -9,16 +9,16 @@ class Song:
 
     def __str__(self):
         #indication of song input status
-        if self.is_required:
+        if self.is_required == 'y':
             print ("{} by {} of year {} now learned.".format(self.title, self.artist, self.year))
         else:
             print ("{} by {} of year {} entered.".format(self.title, self.artist, self.year))
 
-    def mark_learned(self):
-        #check on song 'learned' status
-        if self.is_required:
-            print("Song is already learnt.")
-        else:
-            self.is_required = True
+    # def mark_learned(self):
+    #     #check on song 'learned' status
+    #     if self.is_required == 'y':
+    #         print("Song is already learnt.")
+    #     else:
+    #         self.is_required = 'y'
 
     pass
